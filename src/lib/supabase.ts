4856d6ea04d2fc65380b6b3df4658c2a108523ea
calimiero <mertucar29@gmail.com>
2025-02-25 08:11:42 +0300

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://orecskavelwbthlsmrbs.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9yZWNza2F2ZWx3YnRobHNtcmJzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAyMTgyNTAsImV4cCI6MjA1NTc5NDI1MH0.EFhBN3inBVzZsSzkCUsJy8En0Fgawmvvw-8Aqj_Ddnw';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
